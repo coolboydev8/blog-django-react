7f8e26a91ae54483381fbb5c0a5acc8d1a4cab2b
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-14 13:59:17 +0200

    add NoteSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,3 +13,10 @@ class UserSerializer(serializers.ModelSerializer):
         print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
