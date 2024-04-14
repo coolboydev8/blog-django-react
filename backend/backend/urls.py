3e95b0d2d748b5f6f38a26ce1a23c4aabaddf9aa
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-14 14:13:34 +0200

    add path for api.urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/", include("api.urls")),
 ]
