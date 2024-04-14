c0a10b493000caecd5e5bc4a63ff6efbcf4817ff
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-14 16:06:23 +0200

    fix  jwtDecode  import

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import api from "../api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
 
