a626a0103b270f35f509a395d1595ab24e62963e
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-14 16:10:40 +0200

    add Router & all pages to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,43 @@
 import React from "react";
 import "./styles/reset.css";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Page404 from "./pages/Page404";
 
-function App() {
+const Logout = () => {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+};
+
+const RegisterAndLogout = () => {
+  localStorage.clear();
+  return <Register />;
+};
+
+const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <h1 className="font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 leading-loose">
         Full-stack Blog App
       </h1>
-    </>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
