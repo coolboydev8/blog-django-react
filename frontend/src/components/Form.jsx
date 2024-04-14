5164dc7d6e092f9695d591e3ba0d593b1181a3d3
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-14 18:27:17 +0200

    handle error on using component instead aller

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -8,6 +8,7 @@ const Form = ({ route, pagetype }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const name = pagetype === "login" ? "Login" : "Register";
@@ -26,7 +27,7 @@ const Form = ({ route, pagetype }) => {
         navigate("/login");
       }
     } catch (error) {
-      alert(error);
+      setError(error.message);
     } finally {
       setLoading(false);
     }
@@ -53,6 +54,7 @@ const Form = ({ route, pagetype }) => {
         placeholder="Password"
       />
       {loading && <Loading />}
+      {error && <p className="text-red-500 mb-2">{error}</p>}
       <button
         className="w-56 text-white hover:text-purple-700 hover:bg-white border border-1 border-purple-200 h-10"
         type="submit"
