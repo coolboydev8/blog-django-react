5d96c04dba433f009df2b0f5fe47e57b7718f4b7
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-14 16:13:48 +0200

    add 404 page content

--- frontend/src/pages/Page404.jsx
+++ frontend/src/pages/Page404.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Page404 = () => {
   return (
     <div>
-      <h1 className="text-white text-3xl">404, Not Found Page</h1>
+      <h1 className="text-white text-3xl mb-1">404 Not Found.</h1>
+      <p className="text-white text-md">
+        The page you're looking for doesn't exist.
+      </p>
     </div>
   );
 };
