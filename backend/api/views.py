32c23ad5458010ba6a9efb0d87406b19fe9672d7
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-14 13:50:35 +0200

    add models, views, serializers & urls with migrations

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
