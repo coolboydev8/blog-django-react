1812669727f5a80b73977651b51415c42460220b
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-14 17:41:35 +0200

    add Form component

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import LoadingIndicator from "./LoadingIndicator";
+
+const Form = ({ route, pagetype }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const name = pagetype === "login" ? "Login" : "Register";
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+
+    try {
+      const res = await api.post(route, { username, password });
+      if (pagetype === "login") {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate("/");
+      } else {
+        navigate("/login");
+      }
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="">
+      <h1 className="">{name}</h1>
+      <input
+        className=""
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        className=""
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      {loading && <LoadingIndicator />}
+      <button className="" type="submit">
+        {name}
+      </button>
+    </form>
+  );
+};
+
+export default Form;
