9f1a163b572cea8f8d4dd0c2e932227321c34e07
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-15 12:25:31 +0200

    add register info on login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Form from "../components/Form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
+    <div className="flex flex-col">
       <Form route="/api/token/" pagetype="login" />
+      <p className="flex my-2 justify-center items-center text-xs text-white">
+        Haven't got account? Register{" "}
+        <Link
+          className="flex mx-1 underline cursor-pointer hover:no-underline"
+          to="/register"
+        >
+          here
+        </Link>
+      </p>
     </div>
   );
 };
