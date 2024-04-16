58bb2e09e884f8e0135caf7438e47cd81be9890b
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-16 14:33:43 +0200

    add a new tags component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import api from "../api";
 import Note from "../components/Note";
+import TagsComponent from "../components/TagsComponent";
 
 function Home() {
   const [notes, setNotes] = useState([]);
@@ -91,6 +92,7 @@ function Home() {
               onChange={(e) => setContent(e.target.value)}
             ></textarea>
           </div>
+          <TagsComponent />
           <input
             type="submit"
             value="Submit"
